5f236939ef0735223a47f8118d5b0b49eae58777
Alexei <sheskoalexei@gmail.com>
2023-02-16 16:37:32 +0300

    fixed registration, added account profile

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 
-from accounts.views import SignUpView, PasswordResetView
+from accounts.views import SignUpView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, profile, \
+    LoginView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
-    path('password_reset/', PasswordResetView.as_view(), name='password_reset')
+    path('password_reset/', PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('profile/', profile, name='accounts-profile'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('', include('django.contrib.auth.urls')),
 ]
