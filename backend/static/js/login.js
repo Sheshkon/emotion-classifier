7a6bf1bedffb70518de96dd981adfddbd6b1aba4
Alexei <sheskoalexei@gmail.com>
2023-02-25 16:13:42 +0300

    added login styles

---
+++ backend/static/js/login.js
@@ -0,0 +1,5 @@
+const usernamePlaceholder = document.getElementById('id_username')
+const passwordPlaceholder = document.getElementById('id_password')
+
+usernamePlaceholder.placeholder = 'Username'
+passwordPlaceholder.placeholder = 'Password'
