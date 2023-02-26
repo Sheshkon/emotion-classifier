a2bca8a342aa998811164c1189f3fe479319e511
Alexei <sheskoalexei@gmail.com>
2023-02-26 13:49:13 +0300

    added styles to register and login

---
+++ backend/static/js/accounts/signup.js
@@ -0,0 +1,9 @@
+const usernamePlaceholder = document.getElementById('id_username')
+const emailPlaceholder = document.getElementById('id_email')
+const password1Placeholder = document.getElementById('id_password1')
+const password2Placeholder = document.getElementById('id_password2')
+
+usernamePlaceholder.placeholder = 'Username'
+password1Placeholder.placeholder = 'Password'
+password2Placeholder.placeholder = 'Confirm Password'
+emailPlaceholder.placeholder = 'Email'
